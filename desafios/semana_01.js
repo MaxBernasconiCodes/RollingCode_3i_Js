f523c9f95c1ad724bd3c45d7fa87f2ae86ca8597
“Max <max_bernasconi@hotmail.com>
2022-06-16 21:23:54 -0300

    correcttion

--- desafios/semana_01.js
+++ desafios/semana_01.js
@@ -1,22 +1,21 @@
 //Sumatoria de numeros :
 // Crear una funcion que tome como parametro un numero y devuelva por consola como resultado la suma de todos los numeros hasta el, por ej:
-// sumarotia(5) devolveria 
+// sumarotia(5) devolveria  13
 // por que  1 + 2 + 3 + 4 + 5 = 13;
-// sumarotia(10) devolveria 
+// sumarotia(10) devolveria  55
 // por que  1 + 2 + 3 + ... + 10 = 55
 /*--------------------------------------------------------------*/
-
-
+function sumarotia(numero) {}
 
 
 
 /*--------------------------------------------------------------*/
 //Tabla de multiplicar
 // Crear una funcion que devuelva un array nuevo en donde esten los multiplos del 1 al 10 de el numero pasado y mostrarlo por consola, por ej:
-// tabla de (5) devolveria [5,10,15,20,25,30,35,40,45,50]
-// tabla de (52) devolveria [2,4,6,8,10,12,14,16,18,20]
+// tablaDe(5) devolveria [5,10,15,20,25,30,35,40,45,50]
+// tablaDe(2) devolveria [2,4,6,8,10,12,14,16,18,20]
 /*--------------------------------------------------------------*/
-
+function tablaDe(numero) {}
 
 
 
